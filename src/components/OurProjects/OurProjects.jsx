f79dba35a8776072d723a071f0c8ff614fdf3253
unknown <arterchuk789@gmail.com>
2023-10-11 16:57:01 +0300

    change some styles

--- src/components/OurProjects/OurProjects.jsx
+++ src/components/OurProjects/OurProjects.jsx
@@ -6,15 +6,24 @@ const OurProjects = () => {
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.title}>Наші проекти</h2>
-      <Carousel>
+      <Carousel swipe={true}>
         <div className={styles.carousel}>
-          <img src="https://st2.depositphotos.com/1607243/12149/i/600/depositphotos_121494524-stock-photo-responsive-website-design-on-screens.jpg" />
+          <img
+            onClick={(e) => e.preventDefault()}
+            src="https://st2.depositphotos.com/1607243/12149/i/600/depositphotos_121494524-stock-photo-responsive-website-design-on-screens.jpg"
+          />
         </div>
         <div className={styles.carousel}>
-          <img src="https://st2.depositphotos.com/1607243/10716/i/600/depositphotos_107163326-stock-photo-devices-responsive-on-workspace-fashion.jpg" />
+          <img
+            onClick={(e) => e.preventDefault()}
+            src="https://st2.depositphotos.com/1607243/10716/i/600/depositphotos_107163326-stock-photo-devices-responsive-on-workspace-fashion.jpg"
+          />
         </div>
         <div className={styles.carousel}>
-          <img src="https://st2.depositphotos.com/1607243/11313/i/600/depositphotos_113133554-stock-photo-digital-generated-devices-on-desktop.jpg" />
+          <img
+            onClick={(e) => e.preventDefault()}
+            src="https://st2.depositphotos.com/1607243/11313/i/600/depositphotos_113133554-stock-photo-digital-generated-devices-on-desktop.jpg"
+          />
         </div>
       </Carousel>
     </div>
